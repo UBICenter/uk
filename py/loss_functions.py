dce2050276f0cc332f2a5509e264646be5ad1576
Max Ghenis <mghenis@gmail.com>
2020-12-09 23:30:57 -0800

    Add reform_gini loss function

--- py/loss_functions.py
+++ py/loss_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from openfisca_uk.tools.simulation import Simulation
+import microdf as mdf
 
 
 DATA_DIR = "~/frs"
@@ -47,19 +48,19 @@ def loss_metrics(
     :param population: Variable indicating the subpopulation to calculate
         losses for. Defaults to people_in_household, i.e. all people.
     :type population: str, optional
-    :return: Series with four elements:
+    :return: Series with five elements:
         loser_share: Share of the population who come out behind.
         losses: Total losses among losers in pounds.
         mean_pct_loss: Average percent loss across the population
             (including zeroes for people who don't experience losses).
         mean_pct_loss_pwd2: Average percent loss across the population, with
             double weight given to people with disabilities.
+        reform_gini: Gini index of per-person household net income in the
+            reform scenario, weighted by person weight at the household level.
     :rtype: pd.Series
     """
-    change = (
-        reform_sim.calc("household_net_income")
-        - baseline_df.household_net_income
-    )
+    reform_hh_net_income = reform_sim.calc("household_net_income")
+    change = reform_hh_net_income - baseline_df.household_net_income
     loss = np.maximum(-change, 0)
     if population:
         weight_var = baseline_sim.calc(population, map_to="household")
@@ -84,8 +85,21 @@ def loss_metrics(
     )
     total_pop_pwd2 = pwd2_weight.sum()  # Denominator.
     mean_pct_loss_pwd2 = total_pct_loss_pwd2 / total_pop_pwd2
+    # Gini of income per person.
+    reform_hh_net_income_pp = reform_hh_net_income / people_in_household
+    # mdf.gini requires a dataframe.
+    reform_df = pd.DataFrame(
+        {"reform_hh_net_income_pp": reform_hh_net_income_pp, "weight": weight}
+    )
+    reform_gini = mdf.gini(reform_df, "reform_hh_net_income_pp", "weight")
     # Return Series of all metrics.
     return pd.Series(
-        [loser_share, losses, mean_pct_loss, mean_pct_loss_pwd2],
-        index=["loser_share", "losses", "mean_pct_loss", "mean_pct_loss_pwd2"],
+        [loser_share, losses, mean_pct_loss, mean_pct_loss_pwd2, reform_gini],
+        index=[
+            "loser_share",
+            "losses",
+            "mean_pct_loss",
+            "mean_pct_loss_pwd2",
+            "reform_gini",
+        ],
     )
