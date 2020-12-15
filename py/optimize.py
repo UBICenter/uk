00cb9452ad08cf1e4ea98b0b7ac9815bbe1fa2ed
DeepakSingh98 <tejasvisingh@college.harvard.edu>
2020-12-15 19:55:48 +0000

    Added reform argument

--- py/optimize.py
+++ py/optimize.py
@@ -3,8 +3,7 @@ from uk.py.loss_functions import loss_metrics, extract
 from uk.py.calc_ubi import get_data, get_adult_amount
 
 
-
-def optimize(input_dict, loss_metric):
+def optimize(input_dict, loss_metric, reform):
 
   '''
   Arguments:
@@ -22,10 +21,19 @@ def optimize(input_dict, loss_metric):
                 'YORKSHIRE', 'EAST_MIDLANDS', 'WEST_MIDLANDS', 'EAST_OF_ENGLAND', 'LONDON', 
                 'SOUTH_EAST', 'SOUTH_WEST', 'WALES', 'SCOTLAND', 'NORTHERN_IRELAND']
 
-  bounds = [input_dict[i] for i in CATEGORIES]
-
+  # Set bounds according to chosen reform.
+  if reform == 'reform_1':
+    bounds = [input_dict[i] for i in CATEGORIES[:3]]
+    bounds += [(0,0)] * 15
+  elif reform == 'reform_2':
+    bounds = [input_dict[i] for i in CATEGORIES[:6]]
+    bounds += [(0,0)] * 12
+  elif reform == 'reform_3':
+    bounds = [input_dict[i] for i in CATEGORIES]
   
+
   baseline_df, reform_base_df, budget = get_data()
+
 
   # Take the average value of each tuple to create array of starting values
   x = [((i[0] + i[1])/2) for i in bounds]
@@ -33,12 +41,8 @@ def optimize(input_dict, loss_metric):
   def loss_func(x, args=(loss_metric)):
     loss_metric = args
 
-    # Calculate loss with current solution.
+    # Calculate loss with current solution (given the adult amount)
     loss = loss_metrics(x, baseline_df, reform_base_df, budget)[loss_metric]
-
-    # Update the adult amount in x
-    senior, child, dis_1, dis_2, dis_3, regions = extract(x)
-    x[1] = get_adult_amount(reform_base_df, budget, senior, child, dis_1, dis_2, dis_3, regions, individual=True)
 
     # Print loss and corresponding solution set
     output_dict = {CATEGORIES[i]: x[i] for i in range(len(x))}
