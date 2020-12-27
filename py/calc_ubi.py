Fix cost calculation bug

py/calc_ubi.py
@@ -267,12 +267,12 @@ def get_adult_amount(
     for i, region_name in zip(range(len(regions)), REGIONS):
         basic_income += (
             np.where(REGIONS[base_df["region"]] == region_name, regions[i], 0)
+            * base_df["people_in_household"]
             * 52
         )
     total_cost = np.sum(
         basic_income
         * base_df["household_weight"]
-        * base_df["people_in_household"]
     )
     adult_amount = (budget - total_cost) / np.sum(
         base_df["is_WA_adult"] * base_df["household_weight"]