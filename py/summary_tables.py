393c1f3bafad38c74c0436a5ef3c12ea91930034
Max Ghenis <mghenis@gmail.com>
2021-04-28 14:12:29 -0700

    Update summary tables

--- py/summary_tables.py
+++ py/summary_tables.py
@@ -40,11 +40,17 @@ ineq_reform = h.groupby("reform").equiv_household_net_income.agg(INEQS)
 ineq_reform.columns = [i + "_reform" for i in ineq_reform.columns]
 budget_impact = group(h, ["reform"], compare_cols=["household_net_income"])
 budget_impact["household_net_income_chg"] = (
-    budget_impact.household_net_income - budget_impact.household_net_income_base
+    budget_impact.household_net_income
+    - budget_impact.household_net_income_base
 )
 pov = group(p, ["reform"], compare_cols=POVS, mean=True)
 p_agg = p.groupby("reform")[["household_net_income_pl", "winner"]].mean()
-r = p_agg.join(ineq_base).join(ineq_reform, on="reform").join(budget_impact).join(pov)
+r = (
+    p_agg.join(ineq_base)
+    .join(ineq_reform, on="reform")
+    .join(budget_impact)
+    .join(pov)
+)
 r["reform"] = r.index  # Easier for plotting.
 for i in INEQS:
     r[i + "_pc"] = pct_chg(r[i + "_base"], r[i + "_reform"])
@@ -57,7 +63,7 @@ decile["chg"] = decile.household_net_income - decile.household_net_income_base
 decile["weekly_chg_pp"] = (decile.chg / decile.people) / 52
 
 
-G = ["reform", "region_name"]
+G = ["reform", "region"]
 
 region_pov = group(p, G, compare_cols=["in_poverty_bhc"])
 region_inc = group(h, G, compare_cols=["household_net_income"])
@@ -87,7 +93,9 @@ pov_age["pov_chg"] = pov_age.in_poverty_bhc / pov_age.in_poverty_bhc_base - 1
 pov_age["deep_pov_chg"] = (
     pov_age.in_deep_poverty_bhc / pov_age.in_deep_poverty_bhc_base - 1
 )
-pov_chg_long = pov_age.melt(["reform", "age_group"], ["pov_chg", "deep_pov_chg"])
+pov_chg_long = pov_age.melt(
+    ["reform", "age_group"], ["pov_chg", "deep_pov_chg"]
+)
 
 # Within-decile.
 BUCKETS = [
@@ -97,7 +105,9 @@ BUCKETS = [
     "Gain more than 5%",
 ]
 h["household_net_income_pc_group"] = pd.cut(
-    h.household_net_income_pc, [-np.inf, -0.05, 0, 0.05, np.inf], labels=BUCKETS,
+    h.household_net_income_pc,
+    [-np.inf, -0.05, 0, 0.05, np.inf],
+    labels=BUCKETS,
 )
 
 
@@ -125,12 +135,9 @@ chg_bucket["order"] = chg_bucket.household_net_income_pc_group.map(
 )
 chg_bucket.sort_values("order", ascending=False, inplace=True)
 
-# Poverty by demographic.
-
-
 # Export.
 def csv(df, f):
-    df.to_csv("../data/" + f + ".csv", index=False)
+    df.to_csv("data/" + f + ".csv", index=False)
 
 
 csv(r, "reform")
